12423d7da6c76d11cebb2a8360c026c38469514f
Joe Diton <joediton@netconstruct.co.uk>
2024-02-03 22:32:40 +0000

    Removed redux and auth0 references

--- CD/src/components/Header/Header.tsx
+++ CD/src/components/Header/Header.tsx
@@ -1,34 +1,8 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { Button } from '@mui/material';
 import { FC } from 'react';
 
 const Header: FC = () => {
-    const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
-
-    const handleButtonClick = () => {
-        if (isAuthenticated) {
-            logout({ logoutParams: { returnTo: window.location.origin } });
-        } else {
-            loginWithRedirect();
-        }
-    };
-
     return (
-        <header className='z-50 h-[var(--headerHeight)] dark:bg-[#121212] flex fixed top-0 right-0 left-0 justify-between gap-[30px] items-center p-4'>
-            {isAuthenticated && (
-                <div>
-                    {user?.name}
-                </div>
-            )}
-
-            <Button
-                type="submit"
-                variant="contained"
-                onClick={handleButtonClick}
-            >
-                {isAuthenticated ? "Log out" : "Log in"}
-            </Button>
-        </header>
+        <header />
     );
 };
 
