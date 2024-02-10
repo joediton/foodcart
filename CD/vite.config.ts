2e16f2b71f36296ecbd3b16ed2bd561d14cef880
Joe Diton <joediton@netconstruct.co.uk>
2024-02-10 00:34:01 +0000

    Resolved await error

--- CD/vite.config.ts
+++ CD/vite.config.ts
@@ -5,8 +5,8 @@ import { resolve } from "path";
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    }
+      "@": resolve(__dirname, "src"),
+    },
   },
   optimizeDeps: {
     esbuildOptions: {
@@ -16,6 +16,9 @@ export default defineConfig({
   esbuild: {
     // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
     logOverride: { "this-is-undefined-in-esm": "silent" },
+    supported: {
+      "top-level-await": true, //browsers can handle top-level-await features
+    },
   },
   plugins: [
     react({
